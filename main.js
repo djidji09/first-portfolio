9c31a45859929de529810488126fca6172864f26
djidji09 <djelouahyoucef1@gmail.com>
2023-07-04 10:24:15 +0200

    making a light and dark mod and togeling between them buttun

---
+++ main.js
@@ -0,0 +1,15 @@
+let dark = document.getElementById('dark');
+let light = document.getElementById('light');
+let swatch = document.getElementById('swatch')
+let body = document.body;
+
+//dark.onclick = () => {
+//    body.classList.remove('light');
+//    body.classList.add('dark');
+//};
+//light.onclick = () => {
+//    body.classList.replace('dark', 'light');
+//};
+swatch.onclick = ( ) => {
+    body.classList.toggle('dark');
+};
