d3479ff68f72a4311b9a28c69402d7d93bb22428
djidji09 <djelouahyoucef1@gmail.com>
2023-07-08 09:58:35 +0200

    making a typing effect

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let light = document.getElementById('light');
 let swatch = document.getElementById('swatch')
 let body = document.body;
 
+
 //dark.onclick = () => {
 //    body.classList.remove('light');
 //    body.classList.add('dark');
